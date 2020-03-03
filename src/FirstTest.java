7db387f40d25500c2fc6658dcbc28f68e8b94950
ssomkov <ssomkov@gmail.com>
2020-03-03 18:33:45 +0400

    Initial commit

---
+++ src/FirstTest.java
@@ -0,0 +1,53 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import utility.OsCheck;
+
+import java.net.URL;
+
+public class FirstTest {
+
+    private AppiumDriver driver;
+    private String apkFilePath;
+
+    @Before
+    public void setUP() throws Exception {
+
+        OsCheck.OSType osType = OsCheck.getOperatingSystemType();
+        switch (osType) {
+            case Windows:
+                apkFilePath = "C:\\Users\\ssomk\\IdeaProjects\\JavaAppiumAutomation\\apks\\wiki.apk";
+                break;
+            case MacOS:
+                apkFilePath = "";
+                break;
+            default:
+                apkFilePath = "C:\\Users\\ssomk\\IdeaProjects\\JavaAppiumAutomation\\apks\\wiki.apk";
+                break;
+        }
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "6.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", apkFilePath);
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void firstTest() {
+        System.out.println("First test run");
+    }
+}
