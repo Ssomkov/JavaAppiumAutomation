84cba5ac3d0697bb1fecaf4f933f7dd196b74849
ssomkov <ssomkov@gmail.com>
2020-03-06 14:44:50 +0400

    Ex2

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utility.OsCheck;
 
 import java.net.URL;
@@ -12,6 +17,8 @@ public class FirstTest {
 
     private AppiumDriver driver;
     private String apkFilePath;
+
+    private By searchFieldFirstPagePlaceholder = By.xpath("//*[@resource-id='org.wikipedia:id/search_container']//*[@class='android.widget.TextView']");
 
     @Before
     public void setUP() throws Exception {
@@ -39,6 +46,10 @@ public class FirstTest {
         capabilities.setCapability("app", apkFilePath);
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+        //skip start window
+        WebElement skipButton = driver.findElementByXPath("//*[@resource-id='org.wikipedia:id/fragment_onboarding_skip_button']");
+        skipButton.click();
     }
 
     @After
@@ -47,7 +58,27 @@ public class FirstTest {
     }
 
     @Test
-    public void firstTest() {
-        System.out.println("First test run");
+    public void checkValueSearchStringPlaceholderFirstPage() {
+        String textFromField = waitForElementAndGetText(searchFieldFirstPagePlaceholder,
+                "Поле ввода 'Search string' не найдено", 10);
+        Assert.assertTrue("Значение плейсхолдера в поле ввода 'Search string' отличается от ожидаемого",
+                textFromField.contains("Search"));
+    }
+
+    private WebElement waitForElementPresent(By by, String errorMessage, long timeOutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+        wait.withMessage(errorMessage + "\n");
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private WebElement waitForElementPresent(By by, String errorMessage) {
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.withMessage(errorMessage + "\n");
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private String waitForElementAndGetText(By by, String errorMessage, long timeOutInSeconds) {
+        WebElement element = waitForElementPresent(by, errorMessage, timeOutInSeconds);
+        return element.getText();
     }
 }
